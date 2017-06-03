afe34adf5fbbfce286f536ffedcf813442ac2511
skiden <skiden@huway.com>
2017-06-03 23:32:30 +0800

    init

--- app/Admin/routes.php
+++ app/Admin/routes.php
@@ -5,9 +5,9 @@ use Illuminate\Routing\Router;
 Admin::registerHelpersRoutes();
 
 Route::group([
-    'prefix'        => config('admin.prefix'),
-    'namespace'     => Admin::controllerNamespace(),
-    'middleware'    => ['web', 'admin'],
+    'prefix'     => config('admin.prefix'),
+    'namespace'  => Admin::controllerNamespace(),
+    'middleware' => ['web', 'admin'],
 ], function (Router $router) {
 
     $router->get('/', 'HomeController@index');

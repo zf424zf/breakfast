63cf1722897c06b70c8e721c5582b3cc9613f277
skiden <skiden@huway.com>
2017-06-08 21:44:41 +0800

    wechat SDK

---
+++ app/Http/Controllers/IndexController.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: skiden
+ * Date: 2017/6/8
+ * Time: 下午9:43
+ */
+
+namespace App\Http\Controllers;
+
+
+class IndexController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('wechat.userinfo', ['except' => '']);
+    }
+
+    public function index()
+    {
+
+    }
+}
---
+++ app/Http/Middleware/WechatUserinfo.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: skiden
+ * Date: 2016/12/15
+ * Time: 下午3:13
+ */
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class WechatUserinfo
+{
+
+    const SCOPE = 'snsapi_userinfo';
+
+    /**
+     * Handle an incoming request.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @param \Closure $next
+     * @param string|null $guard
+     *
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        if (!session('wechat.user')) {
+            if ($request->has('state') && $request->has('code')) {
+                session(['wechat.user' => app('wechat')->oauth->user()->getOriginal()]);
+                return redirect()->to($this->getTargetUrl());
+            }
+            return app('wechat')->oauth->scopes([self::SCOPE])->redirect($request->fullUrl());
+        }
+        return $next($request);
+    }
+
+    /**
+     * Build the target business url.
+     *
+     * @param Request $request
+     *
+     * @return string
+     */
+    public function getTargetUrl()
+    {
+        $queries = array_except(request()->all(), ['code', 'state']);
+        return request()->url() . (empty($queries) ? '' : '?' . http_build_query($queries));
+    }
+}

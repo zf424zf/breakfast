7b5f2458771859d6e44d01c1c3315bc94d957201
skiden <skiden@huway.com>
2017-07-13 22:18:19 +0800

    title

--- resources/views/cart.blade.php
+++ resources/views/cart.blade.php
@@ -80,7 +80,6 @@
                             <div class="list-block media-list cart-list">
                                 <ul id="cart-list">
                                     @foreach($products as $product)
-                                        @if(!$product['is_early'] && $product['stock'] > 0)
                                         <li class="item-content" data-place="{{$place->id}}" data-pickuptime="{{$pickuptime['id']}}" data-date="{{$date}}" data-count="{{$cart[$product['id']] or 0}}" data-id="{{$product['id']}}">
                                             <div class="item-media">
                                                 <a href="javascript:;" class="food-alert">
@@ -106,7 +105,6 @@
                                                 </div>
                                             </div>
                                         </li>
-                                        @endif
                                     @endforeach
                                 </ul>
                             </div>

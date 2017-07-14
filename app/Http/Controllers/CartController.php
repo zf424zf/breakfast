80eeeb08ae3bdd95a98a8dcf3e191a07f6c265ab
skiden <skiden@huway.com>
2017-07-14 21:58:42 +0800

    title

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -152,6 +152,8 @@ class CartController extends Controller
         }
         $data = session(self::SESSION_KEY, []);
         $product = current(ProductService::gets([], request('date'), request('place_id'), request('pickuptime_id')));
+        var_dump($product);
+        die;
         if (!$product['is_early'] &&
             $product['stock'] < request('count') &&
             request('is_add')
